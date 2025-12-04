613e636f68b87d0a91b37c5a60de03ee14a86623
PPz <ppzreboot@qq.com>
2025-12-04 17:34:14 +0800

    feat(app/client): style of login page

--- app/client/src/page/login/page.tsx
+++ app/client/src/page/login/page.tsx
@@ -1,4 +1,12 @@
 export
 function Login_page(props: { github_oauth_link: string }) {
-	return <a href={props.github_oauth_link}>Login with GitHub</a>
+	return <a
+		href={props.github_oauth_link}
+		style={{
+			position: 'absolute',
+			top: '35%',
+			left: '50%',
+			transform: 'translate(-50%, -50%)',
+		}}
+	>Login with GitHub</a>
 }
