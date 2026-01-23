c902c6b80552d723259b97bdf0c0a9d2768bf930
PPz <ppzreboot@qq.com>
2026-01-23 15:16:08 +0800

    feat(clite): style of vocabulary

--- biz/clite-app/src/page/vocabulary/list/index.ts
+++ biz/clite-app/src/page/vocabulary/list/index.ts
@@ -1,6 +1,7 @@
 import { css } from 'goober'
 import { $S, h, text } from '@biz/c/superfine'
 import { load, page_state } from '../ss.ts'
+import { home_page_url } from '@biz/common/page'
 
 export
 const List = () =>
@@ -9,9 +10,16 @@ const List = () =>
 			...page_state.list.map(item =>
 				h('li',
 					{
+						className: 'voc-item',
 						key: item.record.id,
 					},
-					text(item.record.word),
+					h('a',
+						{
+							href: home_page_url(item.record.word),
+							className: 'reset',
+						},
+						text(item.record.word),
+					),
 				)
 			),
 			h('li', { className: 'load-more' },
@@ -29,5 +37,26 @@ const List = () =>
 	])
 
 const $Cont = $S('div', css({
-
+	padding: '1.2em 0',
+	ul: {
+		display: 'flex',
+		justifyContent: 'center',
+		flexWrap: 'wrap',
+		rowGap: '.6em',
+		columnGap: '1em',
+		li: {
+			backgroundColor: 'var(--content-bg-color)',
+			padding: '0 1.2em',
+			height: '2.6em',
+			borderRadius: '1.3em',
+			display: 'flex',
+			alignItems: 'center',
+			'&.voc-item': {
+				a: {
+					color: 'inherit',
+					textDecoration: 'none',
+				},
+			},
+		},
+	},
 }))
