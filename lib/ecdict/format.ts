c40a2f5d98ab7c28da2cf6c97ec3118e17f77f77
PPz <ppzreboot@qq.com>
2026-03-02 11:17:12 +0800

    feat: ecdict

---
+++ lib/ecdict/format.ts
@@ -0,0 +1,77 @@
+import type { I_ecdict_raw, I_ecdict, I_exchange_type, I_inflection_type } from './type'
+
+export
+function format(record: I_ecdict_raw): I_ecdict {
+    const exchange = (record.exchange === null || record.exchange === '')
+        ? null
+        : Object.fromEntries(
+            (record.exchange as string)
+                .split('/')
+                .map(kv => kv.split(':'))
+        ) as Record<I_exchange_type, string | undefined>
+
+    if (exchange !== null && exchange['0'] !== undefined && exchange['1'] === undefined) {
+        console.error('has a lemma, no inflection type', record.word, record.exchange)
+        // 有的单词有 lemma，但没有 inflection type，比如 `we`
+        exchange[0] = undefined
+    }
+
+    // 相信数据库，不检查值，只格式化值
+    return {
+        word: record.word,
+        phonetic: record.phonetic || null,
+        definition: _strarr_or_null(record.definition),
+        translation: _strarr_or_null(record.translation),
+        // @ts-ignore: 相信数据库
+        collins: record.collins || null, // record.collins: 0, 1, 2, 3, 4, 5
+        oxford: record.oxford === 1, // record.oxford: null, 1
+        bnc: record.bnc || null,
+        frq: record.frq || null,
+        lemma: (exchange === null || exchange['0'] === undefined)
+            ? null
+            : {
+                lemma: exchange['0'],
+                type: (exchange['1'] as string).split('').map(t => ({
+                    p: 'did',
+                    d: 'done',
+                    i: 'ing',
+                    3: 'does',
+                    r: 'er',
+                    t: 'est',
+                    s: 's',
+                }[t as 'p' | 'd' | 'i' | '3' | 'r' | 't' | 's']) as I_inflection_type),
+            }
+        ,
+        inflection: (record.exchange === null || record.exchange === '')
+            ? {} as Record<I_inflection_type, undefined>
+            : Object.fromEntries(
+                (record.exchange as string)
+                    .split('/')
+                    .map(kv => kv.split(':'))
+                    .filter(([k]) => {
+                        if (['p', 'd', 'i', '3', 'r', 't', 's'].includes(k))
+                            return true
+                        if (k !== '0' && k !== '1')
+                            console.error('invalid inflection type detected', record.word, record.exchange)
+                        return false
+                    })
+                    .map(([k, v]) => [
+                        {
+                            p: 'did',
+                            d: 'done',
+                            i: 'ing',
+                            3: 'does',
+                            r: 'er',
+                            t: 'est',
+                            s: 's',
+                        }[k],
+                        v,
+                    ])
+            ) as Record<I_inflection_type, string>,
+    }
+}
+
+function _strarr_or_null(str: string | null): string[] {
+    const trim = (str: string) => str.trim()
+    return str === null ? [] : str.split('\n').map(trim).filter(trim) // 相信数据库
+}
---
+++ lib/ecdict/index.ts
@@ -0,0 +1,14 @@
+import { Database } from 'better-sqlite3'
+import { I_lookup_from_ECDICT, I_ecdict_raw } from './type'
+import { format } from './format'
+
+export
+function ecdict_sqlite3(db: Database): I_lookup_from_ECDICT {
+    const stmt = db.prepare('select * from stardict where word=?')
+    return async function lookup_from_ecdict(word) {
+        if (word.length > 100 || word.length === 0)
+            return null
+        const result = await stmt.get(word) as I_ecdict_raw
+        return result ? format(result) : null // 相信数据库
+    }
+}
